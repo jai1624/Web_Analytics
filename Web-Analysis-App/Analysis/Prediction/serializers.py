0aa95c030407f173c432dd447d38e17ce6c1b474
jai1624 <58846291+jai1624@users.noreply.github.com>
2024-01-19 15:50:27 +0530

    Add files via upload

---
+++ Web-Analysis-App/Analysis/Prediction/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import PredictionResult
+
+
+class PredictionResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PredictionResult
+        fields = '__all__'
